ca02c5db05e95445bef2436b41548d220b740416
Xtarachiever <estheradeyemi796@gmail.com>
2022-07-01 12:21:36 +0100

    minor changes

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -29,7 +29,7 @@ function Content() {
             <span className='px-2'>Remember me</span>
             </label>
           </div>
-          <Button btn="btn-primary" name="Sign in" />
+          <Button btn="btn-primary" name="Sign in" className="sign-up"/>
         </div>
         <div
           className="d-flex align-items-center justify-content-start w-100 mt-3"
