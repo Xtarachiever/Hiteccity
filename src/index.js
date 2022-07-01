df16570e815aae35a2d1bd1f250f7aaf9cc3b22b
Xtarachiever <estheradeyemi796@gmail.com>
2022-07-01 11:45:49 +0100

    implemented boostrap 5.0

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+//Boostrap components
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import './index.css';
 import App from './App';
 
